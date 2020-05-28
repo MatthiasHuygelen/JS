867f7b0fa939fd4a6237be8ab39e59ab59d33002
Matthias Huygelen <Matthias.Huygelen@realdolmen.com>
2020-05-28 15:47:10 +0200

    .

--- hangman/modules/dataService.js
+++ hangman/modules/dataService.js
@@ -1,7 +1,7 @@
 import { randomInteger } from "./util.js"
 
-export async function getWord(){
-    return await fetch("https://api.datamuse.com/words?topics=temperature")
+export function getWord(){
+    return fetch("https://api.datamuse.com/words?topics=temperature")
     .then(data => data.json())
     .then(x => {
         return x[randomInteger(1 , 99)].word.split("");
